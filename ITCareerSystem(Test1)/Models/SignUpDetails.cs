d0469fd382a5e63c9520b550f96c99aa3a08574e
pasanpinsara-git <arachchi11723@usci.ruh.ac.lk>
2024-03-05 12:25:10 +0530

    Create signup controller page

---
+++ ITCareerSystem(Test1)/Models/SignUpDetails.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITCareerSystem_Test1_.Models
+{
+    public class SignUpDetails
+    {
+        [Key]
+        public string? User_Name { get; set; }
+        [Required]
+        public string? Password { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? TP_Number { get; set; }
+
+       
+
+
+
+    }
+}
